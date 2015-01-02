5ea603fcabc06a128a56c9f122f165dced3e376c
Paul I Wilson <paul.i.wilson@ft.com>
2015-01-02 18:33:26 +0000

    fixing build

--- test/fixtures/app/main.js
+++ test/fixtures/app/main.js
@@ -22,7 +22,8 @@ app.get('/templated', function(req, res, next) {
 	res.render('main', {
 		title: "FT",
 		image: "https://avatars0.githubusercontent.com/u/3502508?v=3",
-		date: new Date('Fri Aug 01 2014 00:00:00 GMT')
+		date: new Date('Fri Aug 01 2014 00:00:00 GMT'),
+		text : "<p>Paragraph 1</p><p>Paragraph 2</p><p>Paragraph 3</p>"
 	});
 });
 
