1bd19ac5a6d078c28a52c3e27b140b4e0d3f1b80
Paul I Wilson <paul.i.wilson@ft.com>
2015-01-02 18:01:37 +0000

    changing paragraphs helper to use named arguments and accept text as input

--- src/paragraphs.js
+++ src/paragraphs.js
@@ -1,10 +1,13 @@
 'use strict';
 
-module.exports = function(start, end, options) {
-	var pars = options.fn(this).split('</p>');
-	start = start || 0;
-	end = end || pars.length;
-	return pars.slice(start, end).filter(function(p) {
+module.exports = function(input, options) {
+
+	var text = input instanceof Array ? input.join('') : input,
+		paras = text.split('</p>'),
+		start = options.hash.start || 0,
+		end = options.hash.end || paras.length;
+
+	return paras.slice(start, end).filter(function(p) {
 		return p.length > 0;
 	}).concat(['']).join('</p>');
 };
