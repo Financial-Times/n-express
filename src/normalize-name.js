75b6c48e3d1256951b16a68a84ff74d76b469e01
Matthew Andrews <matt@mattandre.ws>
2015-01-19 23:09:46 +0000

    Lazy is a better solution

--- src/normalize-name.js
+++ src/normalize-name.js
@@ -1,8 +1,7 @@
 "use strict";
 
 module.exports = function(name) {
-	var matches = name.match(/^(?:ft-)?(?:next-)?(.*)(?:-v[0-9]{3,})$/);
-	matches = matches || name.match(/^(?:ft-)?(?:next-)?(.*)/);
+	var matches = name.match(/^(?:ft-)?(?:next-)?(.*?)(?:-v[0-9]{3,})?$/);
 	if (matches) {
 		return matches[1];
 	}
