8e192880ec569f528bc1e23bfb827481237d61ad
Matthew Andrews <matt@mattandre.ws>
2015-01-29 17:15:10 +0000

    add slashes

--- src/handlebars/hashed-asset.js
+++ src/handlebars/hashed-asset.js
@@ -3,7 +3,7 @@ module.exports = function(options) {
 	var assetHash;
 	var file = options.fn(this);
 	try {
-		assetHash = require(options.data.root.__rootDirectory + 'public/' + file + '-asset-hash.json')[file];
+		assetHash = require(options.data.root.__rootDirectory + '/public/' + file + '-asset-hash.json')[file];
 	} catch(err) {
 		if (process.env.NODE_ENV === 'production') {
 			throw new Error("./public/" + file + "-asset-hash.json not found, required for proper working of the application in production");
