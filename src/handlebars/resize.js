ed54a484b5a6d9bec276f938efe13ac8b1b86f1f
Matt Andrews <matt@mattandre.ws>
2015-02-03 13:54:19 +0000

    SSL compatible image service endpoint

--- src/handlebars/resize.js
+++ src/handlebars/resize.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(width, options) {
-	return '//image.webservices.ft.com/v1/images/raw/' + encodeURIComponent(options.fn(this)) + '?width=' + width + '&source=docs&fit=scale-down';
+	return '//imageservice.glb.ft.com/v1/images/raw/' + encodeURIComponent(options.fn(this)) + '?width=' + width + '&source=docs&fit=scale-down';
 };
