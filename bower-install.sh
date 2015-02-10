d24639277518705b9627f5d5ddaf2dbbf3df9293
Keith Rogers <dev@keithrogers.eu>
2015-02-10 15:01:00 +0000

    @latest for wrapper & header

--- bower-install.sh
+++ bower-install.sh
@@ -1,3 +1,3 @@
 bower install -S Financial-Times/next-mustard#^2.0.0 \
- Financial-Times/next-wrapper#^5.3.0 \
- Financial-Times/next-header#^4.0.0
+ Financial-Times/next-wrapper \
+ Financial-Times/next-header
