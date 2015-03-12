cf64efe57e44ecb56fe4cacc26a416f4bfc74063
Matt Andrews <matt@mattandre.ws>
2015-03-12 16:15:14 +0000

    Update resize.js

--- src/handlebars/resize.js
+++ src/handlebars/resize.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(width, options) {
+	
+	// TODO swap ‘docs’ with the app name
 	return '//image.webservices.ft.com/v1/images/raw/' + encodeURIComponent(options.fn(this)) + '?width=' + width + '&source=docs&fit=scale-down';
 };
