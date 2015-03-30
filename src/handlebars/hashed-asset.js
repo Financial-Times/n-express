414df80f7ccaa9ec755c839f3f1fff9aa328944b
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-03-30 10:22:35 +0100

    using next-logger

--- src/handlebars/hashed-asset.js
+++ src/handlebars/hashed-asset.js
@@ -1,25 +1,27 @@
 /*jshint node:true*/
 'use strict';
 
+var logger = require('ft-next-logger');
+
 module.exports = function(options) {
 	var assetHash;
 	var file = options.fn(this);
 	var fallback = '/' + options.data.root.__name+'/' + file;
 	var prefix = (options.data.root.flags.s3.isSwitchedOn ? '' : 'next-') + 'hashed-assets';
 	if (process.env.NODE_ENV !== 'production') {
-		console.log("Asset hashing is only switched on when NODE_ENV is production");
+		logger.info("Asset hashing is only switched on when NODE_ENV is production");
 		return fallback;
 	}
 
 	if (!options.data.root.flags.assetHashing.isSwitchedOn) {
-		console.warn("Asset Hashing flag off, falling back to un-fingerprinted files");
+		logger.warn("Asset Hashing flag off, falling back to un-fingerprinted files");
 		return fallback;
 	}
 
 	try {
 		assetHash = require(options.data.root.__rootDirectory + '/public/asset-hashes.json')[file];
 	} catch(err) {
-		console.warn("./public/asset-hashes.json not found.  Falling back to un-fingerprinted files.");
+		logger.warn("./public/asset-hashes.json not found.  Falling back to un-fingerprinted files.");
 	}
 	if (assetHash) {
 		return '/' + prefix + '/' + options.data.root.__name + '/' + assetHash;
