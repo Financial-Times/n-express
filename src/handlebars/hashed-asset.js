4bafc94d89fb35f563f53801b5a7b2496dfb8304
Matthew Andrews <matt@mattandre.ws>
2015-04-07 11:37:21 +0100

    Logic wrong way round

--- src/handlebars/hashed-asset.js
+++ src/handlebars/hashed-asset.js
@@ -5,7 +5,7 @@ module.exports = function(options) {
 	var assetHash;
 	var file = options.fn(this);
 	var fallback = '/' + options.data.root.__name+'/' + file;
-	var prefix = (options.data.root.flags.s3.isSwitchedOn ? 'next-' : '') + 'hashed-assets';
+	var prefix = (options.data.root.flags.s3.isSwitchedOn ? '' : 'next-') + 'hashed-assets';
 	if (process.env.NODE_ENV !== 'production') {
 		console.log("Asset hashing is only switched on when NODE_ENV is production");
 		return fallback;
