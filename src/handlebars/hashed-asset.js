4f21be0bf9dedc6a7d0f5fd7b343c44676e646a2
Matthew Andrews <matt@mattandre.ws>
2015-04-08 12:05:42 +0100

    Autocomplete is not my friend

--- src/handlebars/hashed-asset.js
+++ src/handlebars/hashed-asset.js
@@ -7,7 +7,7 @@ module.exports = function(options) {
 	var assetHash;
 	var file = options.fn(this);
 	var fallback = '/' + options.data.root.__name+'/' + file;
-	if (process.env.NODE_ENV !== 'production' && production.env.NODE_ENV !== 'branch') {
+	if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'branch') {
 		logger.info("Asset hashing is only switched on when NODE_ENV is production or branch");
 		return fallback;
 	}
