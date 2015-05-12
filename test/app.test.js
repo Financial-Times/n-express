362e86353b290666bd3795135d0ce8eed60ecf41
Rhys Evans <rhys.evans@ft.com>
2015-05-12 12:42:02 +0100

    updated to flags 7.x

--- test/app.test.js
+++ test/app.test.js
@@ -188,7 +188,7 @@ describe('simple app', function() {
 			request(app)
 				.get('/wrapped')
 				// doctype ... header ... script loader ... end page
-				.expect(200, /^<!DOCTYPE html>(.|[\r\n])*header(.|[\r\n])*script-loader(.|[\r\n])*<\/html>/, done);
+				.expect(200, /^<!DOCTYPE html>(.|[\r\n])*header(.|[\r\n])*flags(.|[\r\n])*script-loader(.|[\r\n])*<\/html>/, done);
 		});
 
 		it('wrapper should expose app name to client side code', function(done) {
@@ -207,7 +207,7 @@ describe('simple app', function() {
 			request(app)
 				.get('/vanilla')
 				// doctype ... no header ... script loader ... tracking ... end page
-				.expect(200, /^<!DOCTYPE html>(.|[\r\n])*<body class="o-hoverable-on">([^a-z])*<h1>(.|[\r\n])*script-loader([\t]+)tracking*/, done);
+				.expect(200, /^<!DOCTYPE html>(.|[\r\n])*<body class="o-hoverable-on">([^a-z])*<h1>(.|[\r\n])*flags(.|[\r\n])*script-loader([\t]+)tracking*/, done);
 		});
 
 		it('vanilla should expose app name to client side code', function(done) {
