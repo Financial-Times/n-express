f84e9bd6e06580cbb1779e650d9bba75fd95104e
Rhys Evans <wheresrhys@gmail.com>
2015-06-10 16:00:05 +0100

    added v1 elastic search

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ var serviceMatchers = {
 	'sapi': /^https?:\/\/api\.ft\.com\/content\/search\/v1/,
 	'elastic-v1-article': /^https?:\/\/[\w\-]+\.foundcluster\.com:9243\/v1_api_v2\/item/,
 	'elastic-v2-article': /^https?:\/\/[\w\-]+\.foundcluster\.com:9243\/v2_api_v[12]\/item/,
+	'elastic-v1-search': /^https?:\/\/[\w\-]+\.foundcluster\.com:9243\/v1_api_v2\/_search/,
 	'user-prefs': /^https?:\/\/ft-next-api-user-prefs-v002\.herokuapp\.com/,
 	'flags': /^https?:\/\/ft-next-feature-flags-prod\.s3-website-eu-west-1\.amazonaws\.com\/flags\/__flags\.json$/,
 	// 'elastic-search':
