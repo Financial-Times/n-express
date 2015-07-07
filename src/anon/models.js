e557f14944b1f6d07ec794c2dfdef35ba926156c
Adam Silver <adambsilver@gmail.com>
2015-07-07 14:51:49 +0100

    Fcf link

--- src/anon/models.js
+++ src/anon/models.js
@@ -6,7 +6,7 @@ function AnonymousModel(req){
 
 function FirstClickFreeModel(){
 	this.signInLink = 'https://next.ft.com/login';
-	this.subscribeNowLink = 'https://sub.ft.com/spa_5/';
+	this.subscribeNowLink = 'https://sub.ft.com/spa_5/?segID=400872&segmentID=676c655f-9b47-27a8-97db-ab3a6a6dbc54';
 }
 
 module.exports = {
