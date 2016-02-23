a7929cec6777dc55cfb68149ebf292d571bde1c4
Matthew Andrews <matt@mattandre.ws>
2016-02-23 16:25:52 +0000

    Log more splunkable data when starting up

--- main.js
+++ main.js
@@ -210,7 +210,7 @@ module.exports = function(options) {
 		var port = args[0];
 		var cb = args[1];
 		args[1] = function () {
-			nextLogger.info(`App ${name} listening on ${port}`)
+			nextLogger.info({ event: 'EXPRESS_START', app: name, port: port, nodeVersion: process.version })
 			return cb && cb.apply(this, arguments)
 		}
 
