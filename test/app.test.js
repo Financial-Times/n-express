a37181db4e398dbf89cec1c1992b8685d66221f2
phamann <patrickhamann@gmail.com>
2016-04-04 10:47:40 +0100

    Fix ft-anonymous-user header tests

--- test/app.test.js
+++ test/app.test.js
@@ -65,7 +65,7 @@ describe('simple app', function() {
 		it('set default vary headers', function (done) {
 			request(app)
 				.get('/default-vary')
-				.expect('vary', 'country-code, x-flags, x-ft-anonymous-user')
+				.expect('vary', 'country-code, x-flags, ft-anonymous-user')
 				.expect(200, done);
 
 		});
@@ -73,28 +73,28 @@ describe('simple app', function() {
 		it('extend vary header using single value', function (done) {
 			request(app)
 				.get('/single-vary')
-				.expect('vary', 'country-code, x-flags, x-ft-anonymous-user, test-vary')
+				.expect('vary', 'country-code, x-flags, ft-anonymous-user, test-vary')
 				.expect(200, done);
 		});
 
 		it('extend vary header using vary method', function (done) {
 			request(app)
 				.get('/vary-method')
-				.expect('vary', 'country-code, x-flags, x-ft-anonymous-user, test-vary')
+				.expect('vary', 'country-code, x-flags, ft-anonymous-user, test-vary')
 				.expect(200, done);
 		});
 
 		it('extend vary header using array of values', function (done) {
 			request(app)
 				.get('/array-vary')
-				.expect('vary', 'country-code, x-flags, x-ft-anonymous-user, test-vary1, test-vary2')
+				.expect('vary', 'country-code, x-flags, ft-anonymous-user, test-vary1, test-vary2')
 				.expect(200, done);
 		});
 
 		it('won\'t duplicate vary headers', function (done) {
 			request(app)
 				.get('/duplicate-vary')
-				.expect('vary', 'country-code, x-flags, x-ft-anonymous-user')
+				.expect('vary', 'country-code, x-flags, ft-anonymous-user')
 				.expect(200, done);
 		});
 
@@ -102,14 +102,14 @@ describe('simple app', function() {
 			request(app)
 				.get('/multiple-vary')
 				.expect('test-header', 'is-set')
-				.expect('vary', 'country-code, x-flags, x-ft-anonymous-user, test-vary')
+				.expect('vary', 'country-code, x-flags, ft-anonymous-user, test-vary')
 				.expect(200, done);
 		});
 
 		it('unset single vary header', function (done) {
 			request(app)
 				.get('/unset-vary')
-				.expect('vary', 'x-flags, x-ft-anonymous-user')
+				.expect('vary', 'x-flags, ft-anonymous-user')
 				.expect(200, done);
 		});
 
@@ -138,7 +138,7 @@ describe('simple app', function() {
 			request(app)
 				.get('/mixed-vary')
 				.expect('test-header', 'is-set')
-				.expect('vary', 'x-flags, x-ft-anonymous-user, test-vary')
+				.expect('vary', 'x-flags, ft-anonymous-user, test-vary')
 				.expect(200, done);
 		});
 
