f2baae218985b88a2113ae8b5f51e2bc0e963326
Matthew Andrews <matt@mattandre.ws>
2016-04-06 15:30:19 +0100

    Pass the current path to the login link

--- src/navigation/navigationModel.js
+++ src/navigation/navigationModel.js
@@ -1,10 +1,10 @@
 'use strict';
 
-function NavigationModel(flags, isAnon){
+function NavigationModel(flags, isAnon, location){
 	this.myFT = {};
 	this.myAccount = isAnon ? null : {};
 	this.signIn = isAnon ? {} : null;
-	this.signInUrl = '/login';
+	this.signInUrl = `/login?location=${encodeURIComponent(location)}`;
 	this.signOut = !isAnon ? {} : null;
 	this.subscribe = isAnon ? {} : null;
 	this.subscribeUrl = '/products?segID=400863&segmentID=190b4443-dc03-bd53-e79b-b4b6fbd04e64';
