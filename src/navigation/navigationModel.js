dc3d84d1b8bc1af3c01900fdab00bd89506d9be7
Matthew Andrews <matt@mattandre.ws>
2016-04-06 15:35:31 +0100

    Only add ?location is location is set

--- src/navigation/navigationModel.js
+++ src/navigation/navigationModel.js
@@ -4,7 +4,10 @@ function NavigationModel(flags, isAnon, location){
 	this.myFT = {};
 	this.myAccount = isAnon ? null : {};
 	this.signIn = isAnon ? {} : null;
-	this.signInUrl = `/login?location=${encodeURIComponent(location)}`;
+	this.signInUrl = '/login';
+	if (location) {
+		this.signInUrl += `?location=${encodeURIComponent(location)}`;
+	}
 	this.signOut = !isAnon ? {} : null;
 	this.subscribe = isAnon ? {} : null;
 	this.subscribeUrl = '/products?segID=400863&segmentID=190b4443-dc03-bd53-e79b-b4b6fbd04e64';
