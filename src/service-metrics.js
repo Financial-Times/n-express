ca95942bdb124ec17b056464c7434035a2ca5a7d
Rhys Evans <wheresrhys@gmail.com>
2016-05-05 15:32:16 +0100

    changed a const to let (#335)

--- src/service-metrics.js
+++ src/service-metrics.js
@@ -2,9 +2,9 @@
 const raven = require('@financial-times/n-raven');
 const metrics = require('next-metrics');
 const debounce = require('debounce');
-const unregisteredServices = {};
+let unregisteredServices = {};
 
-const getMessage = function () {
+function getMessage() {
 	const message = Object.keys(unregisteredServices).join(', ') + ' services called but no metrics set up. See next-metrics/lib/metrics/services.js';
 	unregisteredServices = {};
 	return message;
@@ -13,15 +13,6 @@ const getMessage = function () {
 const alerter = debounce(function () {
 	raven.captureMessage(getMessage());
 }, 5 * 60 * 1000, true);
-
-/**
-
-	Looking for the services list?
-
-	It's moved to next-metrics
-	https://github.com/Financial-Times/next-metrics/blob/master/lib/metrics/services.js
-
-**/
 
 module.exports = {
 	init: function () {
