89f093dd54c799edd406ecfc703927bc9b6fda79
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-06-23 23:47:44 +0100

    better naming

--- src/middleware/head-css.js
+++ src/middleware/head-css.js
@@ -2,9 +2,10 @@ module.exports = headCssPromise =>
 	(req, res, next) => {
 		headCssPromise
 			.then(headCsses => {
-				res.locals.headCss = headCsses.reduce((headCss, currentHeadCss) => {
-					headCss[currentHeadCss[0]] = currentHeadCss[1];
-					return headCss;
+				// turn the array of arrays into an object, key the filename, value the data
+				res.locals.headCsses = headCsses.reduce((currentHeadCsses, currentHeadCss) => {
+					currentHeadCsses[currentHeadCss[0]] = currentHeadCss[1];
+					return currentHeadCsses;
 				}, {});
 				next();
 			})
