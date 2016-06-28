f4e1ed3dcdda43e3042443a43105fde172a43eba
Laura Carvajal <laura.carvajal@gmail.com>
2016-06-28 10:16:59 +0100

    get brexit discount data to show on marketing promos across the site

--- src/anon/middleware.js
+++ src/anon/middleware.js
@@ -1,6 +1,7 @@
 'use strict';
 const anonModels = require('./models');
 const NavigationModel = require('../navigation/navigationModel');
+const fetchres = require('fetchres');
 
 function showFirstClickFree(req, res){
 	return res.locals.flags && res.locals.flags.firstClickFree &&
@@ -19,7 +20,32 @@ function anonymousMiddleware(req, res, next){
 		|| req.url;
 	res.locals.navigationModel = new NavigationModel(res.locals.flags, res.locals.anon.userIsAnonymous, currentUrl);
 	res.vary('FT-Anonymous-User');
-	next();
+
+	if (res.locals.flags.brexitDiscount && res.locals.flags.brexitDiscountType) {
+		getBrexitDiscountData(req).then(function (response) {
+			res.brexitOfferBasePrice = response.viewData.subscriptionOptions.STANDARD.price.weekly;
+			next();
+		});
+	}
+	else {
+		next();
+	}
+}
+
+function getBrexitDiscountData (req) {
+
+	const headers = {
+		'Content-Classification': req.query['ft-content-classification'] || req.get('FT-Content-Classification') || 'CONDITIONAL_STANDARD',
+		'Country-Code': req.query['country-code'] || req.get('country-code') || 'GBR',
+	};
+
+	if (headers['Content-Classification'] === '-') {
+		headers['Content-Classification'] = 'CONDITIONAL_STANDARD';
+	}
+
+	console.log(headers);
+	return fetch('https://barrier-app.memb.ft.com/memb/barrier/v1/barrier-data', { headers: headers, timeout: 2000 })
+		.then((res) => fetchres.json(res))
 }
 
 module.exports = anonymousMiddleware;
