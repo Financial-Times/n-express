c1872d1bd665917ca069b759936a4b12eb8fbef7
Rhys Evans <rhys.evans@ft.com>
2016-07-28 13:52:59 +0100

    added link headers for hashed assets

--- src/handlebars/hashed-asset.js
+++ src/handlebars/hashed-asset.js
@@ -1,31 +1,27 @@
 'use strict';
 
 const logger = require('@financial-times/n-logger').default;
-let assetHashes;
 
-try {
-	assetHashes = require(`${app.__rootDirectory}/public/asset-hashes.json`);
-} catch(err) {
-	assetHashes = {};
-	logger.warn('./public/asset-hashes.json not found. Falling back to un-fingerprinted files.');
-}
+module.exports = locals => {
+	let assetHashes;
 
-module.exports = app => {
-
-	app.locals.hashedAssets = {};
-	app.locals.hashedAssetPaths = [];
-
-	const slice = [].slice;
-
-	return function(options) {
-		const file = options.fn(this);
-		let url = app.locals.hashedAssets[file];
-		if (!url) {
-			const fallback = `/${app.__name}/${file}`;
-			const hash = assetHashes[file];
-			url = app.locals.hashedAssets[file] =(!app.locals.__isProduction || !hash) ? fallback : `https://next-geebee.ft.com/hashed-assets/${app.locals.__name}/${hash}`;
-			app.locals.hashedAssetPaths = Object.keys(app.locals.hashedAssets).map(k => app.locals.hashedAssets[k]);
-		}
-		return url;
+	try {
+		assetHashes = require(`${locals.__rootDirectory}/public/asset-hashes.json`);
+	} catch(err) {
+		assetHashes = {};
+		logger.warn('./public/asset-hashes.json not found. Falling back to un-fingerprinted files.');
 	}
-};
+
+	function getPath (file) {
+		const fallback = `/${locals.__name}/${file}`;
+		const hash = assetHashes[file];
+		return (!locals.__isProduction || !hash) ? fallback : `https://next-geebee.ft.com/hashed-assets/${locals.__name}/${hash}`;
+	}
+
+	return {
+		helper: function(options) {
+			return getPath(options.fn(this));
+		},
+		getPath: getPath
+	}
+}
---
+++ src/handlebars/index.js
@@ -0,0 +1,49 @@
+const concatHelper = require('./concat');
+const handlebars = require('@financial-times/n-handlebars');
+const hashedAsset = require('./hashed-asset');
+
+module.exports = function (conf) {
+	const app = conf.app;
+	const directory = conf.directory;
+	const options = conf.options;
+	const helpers = options.helpers || {};
+	const hashedAssetUtils = hashedAsset(app.locals);
+
+	helpers.hashedAsset = hashedAssetUtils.helper;
+	helpers.concat = concatHelper;
+
+	app.use((req, res, next) => {
+		const originalRender = res.render;
+		const linkedAssets = [];
+		res.preload = (file, meta) => {
+			meta = meta || {};
+			const header = [];
+			header.push(`<${hashedAssetUtils.getPath(file)}>`);
+			Object.keys(meta).forEach(key => {
+				header.push(`key="${meta[key]}"`)
+			});
+			linkedAssets.push(header.join('; '))
+		}
+
+		res.preload('main.css', {rel: 'stylesheet'});
+		res.preload('main.js');
+
+		res.render = function () {
+			res.set('Link', linkedAssets.join(', '))
+			return originalRender.apply(res, [].slice.call(arguments));
+		}
+		next();
+	})
+
+	return handlebars(app, {
+		partialsDir: [
+			directory + (options.viewsDirectory || '/views') + '/partials'
+		],
+		defaultLayout: false,
+		// The most common use case, n-ui/layout is not bundled with this package
+		layoutsDir: typeof options.layoutsDir !== 'undefined' ? options.layoutsDir : (directory + '/bower_components/n-ui/layout'),
+		helpers: helpers,
+		directory: directory,
+		viewsDirectory: options.viewsDirectory
+	});
+}
