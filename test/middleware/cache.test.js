5dec85577af56d2113996883bef72088dfb03410
Rhys Evans <wheresrhys@gmail.com>
2016-08-24 11:21:05 +0100

    Update cache.test.js

--- test/middleware/cache.test.js
+++ test/middleware/cache.test.js
@@ -15,7 +15,7 @@ describe('cache constants', function () {
 			.expect('FT_SHORT_CACHE', 'max-age=600, stale-while-revalidate=60, stale-if-error=86400')
 			.expect('FT_HOUR_CACHE', 'max-age=3600, stale-while-revalidate=60, stale-if-error=86400')
 			.expect('FT_DAY_CACHE', 'max-age=86400, stale-while-revalidate=60, stale-if-error=86400')
-			.expect('FT_WEEK_CACHE','max-age=604800, stale-while-revalidate=60, stale-if-error=86400')
+			.expect('FT_WEEK_CACHE','max-age=604800, stale-while-revalidate=60, stale-if-error=259200')
 			.expect('FT_LONG_CACHE', 'max-age=86400, stale-while-revalidate=60, stale-if-error=259200')
 			.expect(200, done);
 	});
