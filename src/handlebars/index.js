0b0414636f6b54b346057ebda27fbb278e413996
Rhys Evans <rhys.evans@ft.com>
2016-12-07 12:11:17 +0000

    WIP

--- src/handlebars/index.js
+++ src/handlebars/index.js
@@ -1,5 +1,6 @@
 const handlebars = require('@financial-times/n-handlebars');
 const hashedAssets = require('../lib/hashed-assets');
+const Poller = require('ft-poller');
 
 module.exports = function (conf) {
 	const app = conf.app;
@@ -26,6 +27,21 @@ module.exports = function (conf) {
 		layoutsDir: typeof options.layoutsDir !== 'undefined' ? options.layoutsDir : (directory + '/bower_components/n-ui/layout'),
 		helpers: helpers,
 		directory: directory,
-		viewsDirectory: options.viewsDirectory
-	});
+		viewsDirectory: options.viewsDirectory,
+		cache: true
+	})
+		.then(instance => {
+			new Poller({
+				url: 'http://ft-next-n-ui-prod.s3-website-eu-west-1.amazonaws.com/n-ui/layouts/wrapper-precompiled.js',
+				interval: 5000,
+				parseData: tpl => {
+					console.log('got new tpl')
+					tpl = tpl.replace('</body>', `<div style="background: white;position: absolute;top: 0;left:0;color:red;font-size:50px">${Date.now()}</div></body>`)
+					const tplAsObj = eval(tpl);
+					instance.compiled[typeof options.layoutsDir !== 'undefined' ? options.layoutsDir : (directory + '/bower_components/n-ui/layout') + '/wrapper.html']
+						= instance.handlebars.compile(tplAsObj);
+				}
+			})
+
+		});
 }
