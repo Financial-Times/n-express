f78eb75bebba5878933127779efe4a7e1d0c34e4
Paul Wilson <paul.i.wilson@ft.com>
2016-12-14 14:30:12 +0000

    adding new welcome banner for users in the comact view

--- test/welcome-banner/welcome-banner-model.test.js
+++ test/welcome-banner/welcome-banner-model.test.js
@@ -13,16 +13,6 @@ describe('Welcome Banner Model', () => {
 
 	const wait = t => new Promise(r => setTimeout(r, t));
 
-	const testProp = (res, prop, model) => {
-		expect(res.locals.welcomeBanner[prop]).to.deep.equal(welcomeBannerModelFactory._banners[model][prop])
-	};
-
-	const test = (res, model) => {
-		for(let prop of ['title', 'strapline', 'ctas']){
-			testProp(res, prop, model);
-		}
-	};
-
 	it('Should provide the welcome banner model by default', () => {
 		const res = {locals:{flags:{}}};
 		const req = {path:'/', get: () => ''};
@@ -31,11 +21,11 @@ describe('Welcome Banner Model', () => {
 		return wait(0).then(() => {
 			sinon.assert.called(next);
 			expect(res.locals).to.have.property('welcomeBanner');
-			test(res, 'defaultWelcomeBannerModel');
+			expect(res.locals.welcomeBanner).to.deep.equal(welcomeBannerModelFactory._banners.defaultWelcomeBannerModel)
 		})
 	});
 
-	it('Should provide the compact view model if the compactView flag is on AND we are on the homepage AND the FT-Cookie-ft-homepage-view is not set to "compact"', () => {
+	it('Should provide the compact advert view model if the compactView flag is on AND we are on the homepage AND the FT-Cookie-ft-homepage-view IS NOT set to "compact"', () => {
 		const res = {locals:{flags:{compactView:true}}};
 		const req = {path:'/', get: () => 'standard'};
 		const next = sinon.spy();
@@ -43,9 +33,21 @@ describe('Welcome Banner Model', () => {
 		return wait(0).then(() => {
 			sinon.assert.called(next);
 			expect(res.locals).to.have.property('welcomeBanner');
-			test(res, 'compactViewWelcomeBannerModel');
+			expect(res.locals.welcomeBanner).to.deep.equal(welcomeBannerModelFactory._banners.compactAdvertWelcomeBannerModel)
 		})
 	});
+
+	it('Should provide the compact view model if the compactView flag is on AND we are on the homepage AND the FT-Cookie-ft-homepage-view IS set to "compact"', () => {
+		const res = {locals:{flags:{compactView:true}}};
+		const req = {path:'/', get: () => 'compact'};
+		const next = sinon.spy();
+		welcomeBannerModelFactory(req, res, next);
+		return wait(0).then(() => {
+			sinon.assert.called(next);
+			expect(res.locals).to.have.property('welcomeBanner');
+			expect(res.locals.welcomeBanner).to.deep.equal(welcomeBannerModelFactory._banners.compactViewWelcomeBannerModel)
+		})
+	})
 
 
 })
