c78dda2101e97d47d2ff46f3438a1da27dbedc00
Rhys Evans <rhys.evans@ft.com>
2017-01-16 13:03:18 +0000

    allow passing backend key in ft-next-backend-key-old header

--- src/middleware/backend-authentication.js
+++ src/middleware/backend-authentication.js
@@ -18,13 +18,18 @@ module.exports = appName => {
 			// allow healthchecks etc. through
 			req.path.indexOf('/__') === 0) {
 			next();
-		} else if (backendKeys.indexOf(req.get('FT-Next-Backend-Key')) !== -1) {
+		} else if (
+			backendKeys.indexOf(req.get('FT-Next-Backend-Key')) > -1 ||
+			backendKeys.indexOf(req.get('FT-Next-Backend-Key-Old')) > -1
+		) {
 			res.set('FT-Backend-Authentication', true);
 			next();
 		} else {
 			res.set('FT-Backend-Authentication', false);
 			if (process.env.NODE_ENV === 'production') {
-				res.sendStatus(401);
+				// NOTE - setting the status text is very important as it's used by the CDN
+				// to trigger stale-if-error if we mess up the key synchronisation again
+				res.status(401).send('Invalid Backend Key');
 			} else {
 				next();
 			}
