0ff4a743bd5c50f785d89abed7507e25152f0206
Ben Fletcher <ben.fletcher@ft.com>
2017-06-21 17:28:30 +0100

    secret squirrel fixes

---
+++ secret-squirrel.js
@@ -0,0 +1,26 @@
+module.exports = {
+	files: {
+		allow: [
+			'cert.pem',
+			'key.pem',
+			'src/assets/robots.txt',
+			'src/assets/teapot.ascii'
+		],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'dWcviesUB03w7mltofQp7fiOpPkzoNyI',
+			'g1Lz9joDwspGf9NmymwGWZTWm1YnDMNAJsvDMBFs',
+			'9S5AYP21OmG2Ed45\\+MCf5PJSGjevnc7aqvg8rGp\\+',
+			'Obukg5ifDAUyHBkItzGbc',
+			'2bWCDsmgyILTIL2ejjfZz79JmWr28\\+U/FQOJSqEb',
+			'HTfDuaW2h9Cnt\\+I6k\\+TOg3Ij5vbh/rxNLwTQmzOW',
+			'/V5CwimQKBgB\\+F8ywd5zQhrTZzz7M6ARUQP7j9b/',
+			'wvvSnjBi0lHELEhDDEjdH/IDrumlgykr4aRa4Guw',
+			'RHWRFPG6rVH1YDP7xIX0q',
+			'/AoGAEVfE\\+pHlAlbEo\\+DtbRcig4qmf1Ja\\+kX7dwI'
+		]
+	}
+};
