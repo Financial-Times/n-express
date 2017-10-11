79b1b630f4016e0187fb9a4c0d4ee0270687f497
Caroline <geek-caroline@users.noreply.github.com>
2017-10-11 15:35:45 +0100

    Correcting default 500 rate check (#475)
    * Fixing default 500 rate check to be a spike check
    * Reverted incorrect alteration of code base owner
     🐿 v2.5.16

--- src/lib/error-rate-check.js
+++ src/lib/error-rate-check.js
@@ -1,26 +1,19 @@
 const nHealth = require('n-health');
 
 const DEFAULT_THRESHOLD = 0.04;
-const DEFAULT_SEVERITY = 3; // TODO: see how flaky this is and consider raising to severity 1 (Theo)
+const DEFAULT_SEVERITY = 3;
 
 module.exports = (appName, opts) => {
 	opts = opts || {};
 	const threshold = opts.threshold || DEFAULT_THRESHOLD;
 	const severity = opts.severity || DEFAULT_SEVERITY;
+	let region = process.env.REGION ? '_' + process.env.REGION : '';
 	return nHealth.runCheck({
-		type: 'graphiteThreshold',
-		metric: `
-			divideSeries(
-				sumSeries(
-					next.heroku.${appName}.web_*${ process.env.REGION ? '_' + process.env.REGION : '' }.express.*_GET.res.status.500.count
-				),
-				sumSeries(
-					next.heroku.${appName}.web_*${ process.env.REGION ? '_' + process.env.REGION : '' }.express.*_GET.res.status.*.count
-				)
-			)
-		`.replace(/\t|\n/g, ''),
+		name: `500 rate for ${appName} is acceptable (default n-express health check)`,
+		type: 'graphiteSpike',
+		numerator: `next.heroku.${appName}.web_*${region}.express.*_GET.res.status.500.count`,
+		divisor: `next.heroku.${appName}.web_*${region}.express.*_GET.res.status.*.count`,
 		threshold,
-		name: `500 rate for ${appName} is acceptable`,
 		severity,
 		businessImpact: `Error rate for the ${appName} app is higher than the acceptable threshold of ${threshold*100}%.`,
 		technicalSummary: `The proportion of 500 responses for the ${appName} application requests across all heroku dynos vs all responses is higher than a threshold of ${threshold*100}%`,
