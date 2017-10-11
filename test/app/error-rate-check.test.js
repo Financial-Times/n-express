042c741111038ec94e75efacde448b749e82bc05
Caroline Handley <caroline.handley@ft.com>
2017-10-11 15:01:49 +0100

    Fixing tests  🐿 v2.5.16

--- test/app/error-rate-check.test.js
+++ test/app/error-rate-check.test.js
@@ -27,16 +27,29 @@ describe('Default error rate check', () => {
 
 	it('should compose correct graphite metric with region', () => {
 		process.env.REGION = 'US';
-		const expectedMetric = 'divideSeries(sumSeries(next.heroku.app-name.web_*_US.express.*_GET.res.status.500.count),sumSeries(next.heroku.app-name.web_*_US.express.*_GET.res.status.*.count))';
+
+		const expectedNumerator = 'next.heroku.app-name.web_*_US.express.*_GET.res.status.500.count';
+		const expectedDivisor = 'next.heroku.app-name.web_*_US.express.*_GET.res.status.*.count';
+
 		subject('app-name');
-		expect(nHealthStub.runCheck).calledWithMatch({ metric: expectedMetric });
+		expect(nHealthStub.runCheck).calledWithMatch({
+			numerator: expectedNumerator,
+			divisor: expectedDivisor
+		});
 	});
 
 	it('should compose correct graphite metric without region', () => {
 		delete process.env.REGION;
-		const expectedMetric = 'divideSeries(sumSeries(next.heroku.app-name.web_*.express.*_GET.res.status.500.count),sumSeries(next.heroku.app-name.web_*.express.*_GET.res.status.*.count))';
+
+		const expectedNumerator = 'next.heroku.app-name.web_*.express.*_GET.res.status.500.count';
+		const expectedDivisor = 'next.heroku.app-name.web_*.express.*_GET.res.status.*.count';
+
 		subject('app-name');
-		expect(nHealthStub.runCheck).calledWithMatch({ metric: expectedMetric });
+
+		expect(nHealthStub.runCheck).calledWithMatch({
+			numerator: expectedNumerator,
+			divisor: expectedDivisor
+		});
 	});
 
 });
