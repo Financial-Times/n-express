ddba45d99b1ad15f1362332eab74cff047a0f83b
Samuel Parkinson <51677+sjparkinson@users.noreply.github.com>
2017-11-16 11:50:36 +0000

    www.ft.com _is_ loaded in iframes.

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
 	res.set('X-Content-Type-Options', 'nosniff');
 	res.set('X-Download-Options', 'noopen');
-	res.set('X-Frame-Options', 'SAMEORIGIN');
 	res.set('X-XSS-Protection', '1; mode=block');
 	next();
 };
