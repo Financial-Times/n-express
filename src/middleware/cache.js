d19b6f6794315c7120e930a24284507d72e22865
Samuel Parkinson <51677+sjparkinson@users.noreply.github.com>
2017-11-28 23:26:37 +0000

    Remove no-store directive.

--- src/middleware/cache.js
+++ src/middleware/cache.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-	res.FT_NO_CACHE = 'max-age=0, no-cache, no-store, must-revalidate';
+	res.FT_NO_CACHE = 'max-age=0, no-cache, must-revalidate';
 	res.FT_SHORT_CACHE = 'max-age=600, stale-while-revalidate=60, stale-if-error=86400';
 	res.FT_HOUR_CACHE = 'max-age=3600, stale-while-revalidate=60, stale-if-error=86400';
 	res.FT_DAY_CACHE = 'max-age=86400, stale-while-revalidate=60, stale-if-error=86400';
