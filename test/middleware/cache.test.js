8159786edb17953c2a28d29e282a61781d53c0b3
Samuel Parkinson <51677+sjparkinson@users.noreply.github.com>
2017-11-28 23:27:21 +0000

    Update cache.test.js

--- test/middleware/cache.test.js
+++ test/middleware/cache.test.js
@@ -7,7 +7,7 @@ describe('cache constants', function () {
 	it('define cache preset constants on the response object', function (done) {
 		request(app)
 			.get('/cache-constants')
-			.expect('FT_NO_CACHE', 'max-age=0, no-cache, no-store, must-revalidate')
+			.expect('FT_NO_CACHE', 'max-age=0, no-cache, must-revalidate')
 			.expect('FT_SHORT_CACHE', 'max-age=600, stale-while-revalidate=60, stale-if-error=86400')
 			.expect('FT_HOUR_CACHE', 'max-age=3600, stale-while-revalidate=60, stale-if-error=86400')
 			.expect('FT_DAY_CACHE', 'max-age=86400, stale-while-revalidate=60, stale-if-error=86400')
