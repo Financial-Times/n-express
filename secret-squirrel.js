bb75d26bdeeac9616ecb1c41bc6ce1a9a6abf311
Dragos Digulescu <dragos.digulescu@iquestgroup.com>
2018-07-30 11:18:40 +0300

    Update secret-squirrel config  🐿 v2.10.0

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -13,6 +13,7 @@ module.exports = {
 		denyOverrides: [
 			'dWcviesUB03w7mltofQp7fiOpPkzoNyI', // cert.pem:12
 			'g1Lz9joDwspGf9NmymwGWZTWm1YnDMNAJsvDMBFs', // cert.pem:13|13
+			'9S5AYP21OmG2Ed45\\+MCf5PJSGjevnc7aqvg8rGp\\+', // cert.pem:16
 			'Obukg5ifDAUyHBkItzGbc', // cert.pem:18
 			'2bWCDsmgyILTIL2ejjfZz79JmWr28\\+U/FQOJSqEb', // key.pem:3
 			'HTfDuaW2h9Cnt\\+I6k\\+TOg3Ij5vbh/rxNLwTQmzOW', // key.pem:5
@@ -20,7 +21,7 @@ module.exports = {
 			'wvvSnjBi0lHELEhDDEjdH/IDrumlgykr4aRa4Guw', // key.pem:22
 			'RHWRFPG6rVH1YDP7xIX0q', // key.pem:22
 			'/AoGAEVfE\\+pHlAlbEo\\+DtbRcig4qmf1Ja\\+kX7dwI', // key.pem:24
-			'9S5AYP21OmG2Ed45\\+MCf5PJSGjevnc7aqvg8rGp\\+'
+			'/document/d/1bILX3O37XmhKOtpWvox9BeZ6RW4' // src/lib/aws-keys-healthcheck.js:104|121
 		]
 	}
 };
