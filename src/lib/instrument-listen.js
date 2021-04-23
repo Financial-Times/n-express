52e1685339547215225fa7b6826be02b0035350b
Kara Brightwell <kara@153.io>
2021-04-23 17:01:46 +0100

    load certificate from app directory

--- src/lib/instrument-listen.js
+++ src/lib/instrument-listen.js
@@ -9,25 +9,21 @@ const path = require('path');
 const fs = require('fs');
 const readFile = denodeify(fs.readFile);
 
-module.exports = (app, meta, initPromises) => {
+module.exports = (app, { initPromises, options }) => {
 	async function createServer(app) {
-		if (process.argv.indexOf('--https') > -1) {
+		if (process.argv.includes('--https')) {
 			const [key, cert] = await Promise.all([
-				readFile(path.resolve(__dirname, '../../key.pem')),
-				readFile(path.resolve(__dirname, '../../cert.pem'))
-			])
+				readFile(path.resolve(process.cwd(), 'self-signed-ssl-key.pem')),
+				readFile(path.resolve(process.cwd(), 'self-signed-ssl-certificate.pem'))
+			]).catch(() => {
+				throw Error('n-express was started with --https, but there\'s no self-signed certificate or key in your app directory. run `npx n-express-generate-certificate` to create one')
+			});
 
 			return https.createServer({ key, cert }, app);
 		} else {
 			return http.createServer(app);
 		}
 	}
-
-	async function actualAppListen(app, ...args) {
-		const server = await createServer(app)
-
-		return server.listen(...args);
-	};
 
 	app.listen = async function (port, callback) {
 		// these middleware are attached in .listen so they're
@@ -66,9 +62,6 @@ module.exports = (app, meta, initPromises) => {
 				throw err;
 			});
 		}
-
-		metrics.count('express.start');
-		return actualAppListen(app, port, wrappedCallback);
 	};
 
 	return app;
