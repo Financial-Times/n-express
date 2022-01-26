c04f1ba5388fb34764c318f1030adad215834cb9
Alex Muller <alex.muller@ft.com>
2022-01-26 11:05:25 +0000

    Switch from graphiteSpike to graphiteThreshold for app restarts
    I think this threshold check will be more reliable than looking for spikes.
    
    The way this check works is a little confusing at first but very good
    for us:
    
    - The wildcard matcher (for example *_EU) will match all dynos for
      a given app
    - Graphite will return multiple series for this query (dyno_1_EU,
      dyno_2_EU, etc)
    - The graphiteThreshold check will iterate over all these series
      and check if any of them are above a given value
    - I have used the summarize function to group these series into
      1 hour buckets, and transformNull to set null data (no restarts) to 0
    - The samplePeriod means that we go back 6 hours (6 metrics per dyno)
    
    I reckon if a dyno restarted more than 2 times in a one hour period that
    would be a good indication of a problem.

--- src/lib/app-restart-check.js
+++ src/lib/app-restart-check.js
@@ -6,7 +6,6 @@
 const nHealth = require('n-health');
 
 const DEFAULT_SEVERITY = 2;
-const DEFAULT_THRESHOLD = 20;
 
 /**
   * @param {string} appName
@@ -16,7 +15,6 @@ const DEFAULT_THRESHOLD = 20;
 module.exports = (appName, opts) => {
 	opts = opts || {};
 	const severity = opts.severity || DEFAULT_SEVERITY;
-	const threshold = opts.threshold || DEFAULT_THRESHOLD;
 
 	let region_name, dyno_matcher;
 
@@ -28,18 +26,19 @@ module.exports = (appName, opts) => {
 		dyno_matcher = '*';
 	}
 
+	const applicationStartMetric = `next.heroku.${appName}.${dyno_matcher}.express.start`;
+
 	return nHealth.runCheck(
 		{
 			id: `${appName}-restarts`,
 			name: `${appName} restart rate is normal (${region_name})`,
-			type: 'graphiteSpike',
-			threshold: threshold,
-			baselinePeriod: '14d',
-			samplePeriod: '1hour',
-			numerator: `next.heroku.${appName}.${dyno_matcher}.express.start`,
+			type: 'graphiteThreshold',
+			threshold: 2,
+			samplePeriod: '6hours',
+			metric: `transformNull(summarize(${applicationStartMetric}, '1h'), 0)`,
 			severity: severity,
-			businessImpact: 'Some part of the next platform has become severly unstable; impact can vary',
-			technicalSummary: `This alert going off means that there has been a noticeable (${threshold} times) spike in app restart rate. The reason can be innocent - merging many PRs in one day, but it also may be caused by an app stuck in a crash-restart loop.`,
+			businessImpact: `Parts of the site that depend on '${appName}' may be unstable`,
+			technicalSummary: `This alert going off means that a dyno is restarting more frequently than we expect. The reason can be innocent - merging many PRs in one day, but it also may be caused by an app stuck in a crash-restart loop.`,
 			panicGuide: 'Check Heroku app metrics for app starts to see if they correspond to PRs getting merged or error spikes. If it is error spikes, investigate for potential errors by checking this app\'s Splunk logs and Grafana dashboards.'
 		}
 	);
